13250dfc659b9bad017969e1ff53c2afd08b9824
Maria'lena Pietri <marialena.pietri@maps-system.com>
2021-12-20 19:00:17 +0100

    fix to netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Main from './components/Main'
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
 
 function App() {
   return (
