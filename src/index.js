f0959419afb86cef5e68356fb05a9e1660ac79e8
Maria'lena Pietri <marialena.pietri@maps-system.com>
2021-12-21 20:12:25 +0100

    fix deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TimekitBooking from 'timekit-booking'
-import $ from 'jquery'
 
 class TestApp extends React.Component {
     componentDidMount() {
